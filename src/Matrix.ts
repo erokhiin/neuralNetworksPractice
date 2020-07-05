c64a5c61049ec466ef056e717f7d4495b7289e86
mariimo <erokhinsergg@gmail.com>
2020-07-05 22:21:02 +0300

    🦶

--- src/Matrix.ts
+++ src/Matrix.ts
@@ -8,27 +8,20 @@ export class Matrix {
   constructor(rows: number, columns: number) {
     this.rows = rows;
     this.columns = columns;
-    this.data = [];
-
-    for (let i = 0; i < this.rows; i++) {
-      this.data[i] = [];
-      for (let j = 0; j < this.columns; j++) {
-        this.data[i][j] = 0;
-      }
-    }
+    this.data = Array(rows).fill(Array(columns).fill(0));
   }
-  
-  mapMatrix(callback: (e: number, i: number, j: number) => number) {
+
+  protected map(callback: (e: number, i: number, j: number) => number) {
     return this.data.map((row, i) => row.map((e, j) => callback(e, i, j)));
   }
 
   static add(a: Matrix, b: Matrix) {
     const result = new Matrix(a.rows, a.columns);
-    result.data = a.mapMatrix((element, i, j) => element + b.data[i][j]);
+    result.data = a.map((element, i, j) => element + b.data[i][j]);
     return result;
   }
   add(n: number) {
-    this.data = this.mapMatrix((element) => element + n);
+    this.data = this.map((element) => element + n);
     return this;
   }
 
@@ -36,31 +29,28 @@ export class Matrix {
     if (a.columns !== b.rows) return;
     const result = new Matrix(a.rows, b.columns);
     result.data = result.data.map((_, i) =>
-      a.data.map((row) =>
-        row.reduce((acc, e, j) => e * b.data[j][i] + acc, 0)
-      )
+      a.data.map((row) => row.reduce((acc, e, j) => e * b.data[j][i] + acc, 0))
     );
     return result;
   }
   multiply(n: number) {
-    this.data = this.mapMatrix((element) => element * n);
+    this.data = this.map((element) => element * n);
     return this;
   }
 
   transpose() {
     const result = new Matrix(this.columns, this.rows);
-    this.mapMatrix((e, i, j) => (result.data[j][i] = e));
-    this.data = result.data;
-    return this;
+    this.map((e, i, j) => (result.data[j][i] = e));
+    return result;
   }
 
   randomize() {
-    this.data = this.mapMatrix(() => ~~random(0, 10));
+    this.data = this.map(() => ~~random(0, 10));
     return this;
   }
 
   print() {
-    console.table(this.data)
-    return this
+    console.table(this.data);
+    return this;
   }
 }
