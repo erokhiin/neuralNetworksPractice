1bb569bd67135e16dd2b43c0224ae5b5c37aca68
erokhiin <erokhiin@icloud.com>
2021-01-08 18:00:31 +0300

    add color predictor 🧚👻🤡💩

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { colorPredictorModule } from "./modules/colorPredictorModule";
 import { linearClasifierModule } from "./modules/linearClasifierModule";
 import { xorModule } from "./modules/xorModule";
 
@@ -5,7 +6,10 @@ const root = document.querySelector("#root")
 
 const linearClasifierNode = root.appendChild(document.createElement("div"))
 const xorNode = root.appendChild(document.createElement("div"))
+const ColorPredictorNode = root.appendChild(document.createElement("div"))
+
 
 linearClasifierModule(linearClasifierNode);
 xorModule(xorNode);
+colorPredictorModule(ColorPredictorNode)
 
